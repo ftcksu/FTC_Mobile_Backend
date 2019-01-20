4682d63b5bc9f9a61b1fee5231ece7218e1886f1
unknown <msaljebreen@stc.com.sa>
2019-01-20 23:41:21 +0300

    init

---
+++ src/main/java/sa/edu/ksu/ftc_backend/FtcBackendApplication.java
@@ -0,0 +1,14 @@
+package sa.edu.ksu.ftc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtcBackendApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/sa/edu/ksu/ftc_backend/FtcBackendApplicationTests.java
@@ -0,0 +1,17 @@
+package sa.edu.ksu.ftc_backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FtcBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
