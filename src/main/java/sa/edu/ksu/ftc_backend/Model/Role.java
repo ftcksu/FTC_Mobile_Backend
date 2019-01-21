332803082c53520cb1c23e1fa282ec8192b4a6f5
Yosif Al-Khlefah <435102878@student.ksu.edu.sa>
2019-01-21 23:04:45 +0300

    Models

---
+++ src/main/java/sa/edu/ksu/ftc_backend/Model/Role.java
@@ -0,0 +1,8 @@
+package sa.edu.ksu.ftc_backend.Model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Role {
+
+}
--- src/main/java/sa/edu/ksu/ftc_backend/Model/User.java
+++ src/main/java/sa/edu/ksu/ftc_backend/Model/User.java
@@ -1,7 +1,28 @@
 package sa.edu.ksu.ftc_backend.Model;
 
 import javax.persistence.Entity;
+import java.util.List;
 
 @Entity
 public class User {
+
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String studentId;
+
+    private List<Role> roles;
+
+    private String email;
+
+    private String password;
+
+    private SocialProfile socialProfile;
+
+    private String deviceId;
+
+    private String bio;
 }
